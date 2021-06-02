5fff58df9f27a2addc4732d87c727af1713cd23a
MikitaSerou <Mikita.serou@gmail.com>
2021-06-02 22:26:13 +0300

    -#-

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,18 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.utils.StringArgsValidator;
+
+@Slf4j
+public class App
 {
     public static void main( String[] args )
     {
+
+        StringArgsValidator f = new StringArgsValidator();
+
+        System.out.println(f.getKek());
         System.out.println( "Hello World!" );
 
         for (String a:
