2dc635ef251af71e1cdd77bc80884e139c0f715f
MikitaSerou <Mikita.serou@gmail.com>
2021-06-02 21:57:16 +0300

    -#-

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        for (String a:
+             args) {
+            System.out.println(a);
+        }
+    }
+}
